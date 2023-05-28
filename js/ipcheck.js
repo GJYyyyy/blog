83afcfb58bf881f58deebbde2be5b83ba58773e1
GJYyyyy <772048440@qq.com>
2023-05-28 10:52:25 +0800

    Site updated: 2023-05-28 10:52:16

--- js/ipcheck.js
+++ js/ipcheck.js
@@ -1,9 +1,1 @@
-function ipcheck() {
-    // 节流
-    if(window._ip_check_timer) return;
-    window._ip_check_timer = setTimeout(() => {
-        fetch('https://czaunsuxqdi5dnt2bf4ot6oxou0cdaoh.lambda-url.us-east-1.on.aws/');
-        window._ip_check_timer = 0;
-    }, 1000)
-}
-ipcheck();
+function ipcheck(){window._ip_check_timer||(window._ip_check_timer=setTimeout(()=>{fetch("https://czaunsuxqdi5dnt2bf4ot6oxou0cdaoh.lambda-url.us-east-1.on.aws/"),window._ip_check_timer=0},1e3))}ipcheck();
